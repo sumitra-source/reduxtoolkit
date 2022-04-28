b0704e84f903e3b65ab5482e382f6cf454fd59bb
Sumitra Shrestha <sumiitrashrestha@gmail.com>
2022-04-28 15:05:19 +0545

    react redux with redux toolkit complete

---
+++ src/features/theme/Theme.js
@@ -0,0 +1,18 @@
+  import { useState } from "react"
+import { useDispatch } from "react-redux"
+import { changeTextColor } from "./themeSlice"
+const Theme = () => {
+        const [color,setColor]=useState("white")
+        const dispatch=useDispatch()
+  
+    return (
+
+    <div>
+    <input className="textbox"type="text" onChange={(e)=>setColor(e.target.value)}/>
+        <button className="button" onClick={()=>{dispatch(changeTextColor(color))}}>Change text color</button>
+        <h1>{color}</h1>
+    </div>
+  )
+}
+
+export default Theme
