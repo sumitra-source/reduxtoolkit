f8d89686ae126ea41eb7af55bb9824eb9b60496d
Sumitra Shrestha <sumiitrashrestha@gmail.com>
2022-04-29 18:00:43 +0545

    api reduxproject

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
-import './App.css';
-import Coin from './features/coin/Coin';
-import Counter from './features/counter/Counter';
-import Theme from './features/theme/Theme';
-
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./components/Home/Home";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import PageNotFound from "./components/PageNotFound/PageNotFound";
+import MovieDetail from "./components/MovieDetail/MovieDetail";
+import "./App.scss";
 
 function App() {
- 
   return (
-    <div className="App">
-  <Counter/>
-  <Coin/>
-  <Theme/>
+    <div className="app">
+      <Router>
+        <Header></Header>
+        <div className="container">
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/movie/:imdbID" component={MovieDetail} />
+            <Route component={PageNotFound} />
+          </Switch>
+        </div>
+        <Footer />
+      </Router>
     </div>
   );
 }
---
+++ src/common/apis/MovieApiKey.js
@@ -0,0 +1,1 @@
+export const APIKey ="eec46176";
